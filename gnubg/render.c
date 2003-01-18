77cc2dc2a98bb6485d374fc39166f82b0f13aa29
gtw <>
2003-01-18 16:26:16 +0000

    Fix cosmetic bug with more than 3 chequers on bar (reported by Nardy Pillards).

--- gnubg/render.c
+++ gnubg/render.c
@@ -1984,7 +1984,7 @@ extern void RenderPips( renderdata *prd, unsigned char *puch0,
 static void PointArea( renderdata *prd, int n, int *px, int *py,
 		       int *pcx, int *pcy ) {
     
-    int c = ( !n || n == 25 ) ? 3 : 5;
+    int c = ( !iPoint || iPoint == 25 ) ? 3 : 5;
     
     *px = aaanPositions[ prd->fClockwise ][ n ][ 0 ] * prd->nSize;
     *py = aaanPositions[ prd->fClockwise ][ n ][ 1 ] * prd->nSize;
