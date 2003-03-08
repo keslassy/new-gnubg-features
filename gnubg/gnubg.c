7e6682f454f66fd9e842c84b84478b0859d0f60d
thyssen <>
2003-03-08 07:57:52 +0000

    define Convert as strdup if libiconv is not available.

--- gnubg/gnubg.c
+++ gnubg/gnubg.c
@@ -7261,6 +7261,8 @@ extern char *
 Convert ( const char *sz, 
           const char *szDestCharset, const char *szSourceCharset ) {
 
+#if HAVE_ICONV
+
   iconv_t id;
   int lIn, lOut, l, rc, nUsed;
   const char *pchIn;
@@ -7334,6 +7336,12 @@ Convert ( const char *sz,
     outputerr ( "iconv_close" );
 
   return pchDest;
+
+#else /* HAVE_ICONV */
+
+  return strdup( sz );
+
+#endif /* ! HAVE_ICONV */
 
 }
 
