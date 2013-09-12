fb3db45162d47e5cc20d91eb38479bf5fbfd70e2
plm <>
2013-09-12 21:51:35 +0000

    Silence compiler warnings

--- gnubg/set.c
+++ gnubg/set.c
@@ -2435,12 +2435,11 @@ extern void
 CommandSetScore(char *sz)
 {
 
-    long int n0, n1;
     moverecord *pmr;
     xmovegameinfo *pmgi;
     const char *pch0, *pch1;
     char *pchEnd0, *pchEnd1;
-    int fCrawford0, fCrawford1, fPostCrawford0, fPostCrawford1;
+    int n0, n1, fCrawford0, fCrawford1, fPostCrawford0, fPostCrawford1;
 
     if ((pch0 = NextToken(&sz)) == 0)
         pch0 = "";
@@ -2448,11 +2447,11 @@ CommandSetScore(char *sz)
     if ((pch1 = NextToken(&sz)) == 0)
         pch1 = "";
 
-    n0 = strtol(pch0, &pchEnd0, 10);
+    n0 = (int) strtol(pch0, &pchEnd0, 10);
     if (pch0 == pchEnd0)
         n0 = INT_MIN;
 
-    n1 = strtol(pch1, &pchEnd1, 10);
+    n1 = (int) strtol(pch1, &pchEnd1, 10);
     if (pch1 == pchEnd1)
         n1 = INT_MIN;
 
