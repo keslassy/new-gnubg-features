889968f7258c04c08c2645ad0ffe725cd92129c1
plm <>
2014-06-28 22:31:24 +0000

    Define signbit() only where it is needed

--- gnubg/lib/sigmoid.h
+++ gnubg/lib/sigmoid.h
@@ -126,6 +126,14 @@ static float e[101] = {
     1993.0370438230298f         /* one extra :-) */
 };
 
+/* signbit() is used below in a somewhat performance sensitive place
+ * If HAVE_DECL_SIGNBIT is false, maybe we should juste compare to 0
+ * instead of using this */
+#if !HAVE_DECL_SIGNBIT
+/* copysign() caters for special IEEE 754 numbers */
+#define signbit(x) (copysign(1, (x)) < 0)
+#endif
+
 /* Calculate an approximation to the sigmoid function 1 / ( 1 + e^x ).
  * This is executed very frequently during neural net evaluation, so
  * careful optimisation here pays off.
