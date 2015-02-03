fc74d7e73df7ed0fa1cad23cc1f720f148b618b6
plm <>
2015-02-03 23:01:21 +0000

    Add missing CVS Id

--- gnubg/board3d/fun3d.h
+++ gnubg/board3d/fun3d.h
@@ -1,4 +1,5 @@
-/* This program is free software; you can redistribute it and/or modify
+/*
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 3 or later of the GNU General Public License as
  * published by the Free Software Foundation.
  *
@@ -9,7 +10,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ */
 
 #ifndef FUN3D_H
 #define FUN3D_H
