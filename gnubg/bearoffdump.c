d6cdd4764303058ec6becbf639df0d04b3332943
plm <>
2015-02-08 15:09:09 +0000

    Silence compiler warnings

--- gnubg/bearoffdump.c
+++ gnubg/bearoffdump.c
@@ -41,12 +41,9 @@ main(int argc, char **argv)
 {
 
     char *filename, *szPosID = NULL;
-    int id = 0;
+    unsigned int id = 0;
     bearoffcontext *pbc;
     char sz[4096];
-    int n;
-    int nUs;
-    int nThem;
     TanBoard anBoard;
 
     GOptionEntry ao[] = {
@@ -84,7 +81,7 @@ main(int argc, char **argv)
     if (!id) {
         printf("Position ID     : %s\n", szPosID);
     } else {
-        printf("Position number : %d\n", id);
+        printf("Position number : %u\n", id);
     }
 
     /* This is needed since we call ReadBearoffFile() from bearoff.c */
@@ -112,7 +109,9 @@ main(int argc, char **argv)
 
         PositionFromID(anBoard, szPosID);
     } else {
-        printf("\n" "Dump of position#: %d\n\n", id);
+        unsigned int n, nUs, nThem;
+
+        printf("\n" "Dump of position#: %u\n\n", id);
 
         n = Combination(pbc->nPoints + pbc->nChequers, pbc->nPoints);
         nUs = id / n;
