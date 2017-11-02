efb68e16c2e48691248215401f3524983d364afd
plm <>
2017-11-02 21:49:05 +0000

    Avoid warnings with some non-gcc compilers

--- gnubg/output.h
+++ gnubg/output.h
@@ -32,20 +32,33 @@ extern void output(const char *sz);
 extern void outputl(const char *sz);
 /* Write a character to stdout/status bar/popup window */
 extern void outputc(const char ch);
+/* Write an error message, perror() style */
+extern void outputerr(const char *sz);
+
+#if defined(HAVE_FUNC_ATTRIBUTE_FORMAT)
 /* Write a string to stdout/status bar/popup window, printf style */
 extern void outputf(const char *sz, ...)
     __attribute__ ((format(printf, 1, 2)));
 /* Write a string to stdout/status bar/popup window, vprintf style */
 extern void outputv(const char *sz, va_list val)
     __attribute__ ((format(printf, 1, 0)));
-/* Write an error message, perror() style */
-extern void outputerr(const char *sz);
 /* Write an error message, fprintf() style */
 extern void outputerrf(const char *sz, ...)
     __attribute__ ((format(printf, 1, 2)));
 /* Write an error message, vfprintf() style */
 extern void outputerrv(const char *sz, va_list val)
     __attribute__ ((format(printf, 1, 0)));
+#else
+/* Write a string to stdout/status bar/popup window, printf style */
+extern void outputf(const char *sz, ...);
+/* Write a string to stdout/status bar/popup window, vprintf style */
+extern void outputv(const char *sz, va_list val);
+/* Write an error message, fprintf() style */
+extern void outputerrf(const char *sz, ...);
+/* Write an error message, vfprintf() style */
+extern void outputerrv(const char *sz, va_list val);
+#endif
+
 /* Signifies that all output for the current command is complete */
 extern void outputx(void);
 /* Temporarily disable outputx() calls */
