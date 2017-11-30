9f2777ff4f8eda4843305a904168b1e113e75de6
plm <>
2017-11-30 22:07:47 +0000

    Consistently use g_alloca for small temporary buffers allocations

--- gnubg/gtkboard.c
+++ gnubg/gtkboard.c
@@ -306,10 +306,9 @@ board_draw_area(cairo_t * cr, gint x, gint y, gint cx, gint cy, BoardData * bd)
 {
     unsigned char *puch;
 
-    puch = malloc(cx * cy * 3);
+    puch = g_alloca(cx * cy * 3);
     RenderArea(bd, puch, x, y, cx, cy);
     draw_rgb_image(cr, puch, x, y, cx, cy);
-    free(puch);
 }
 
 #if GTK_CHECK_VERSION(3,0,0)
