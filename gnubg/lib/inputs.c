0e14b04fbd5f93dcc6bd0ec7e8d6e0db211457b5
plm <>
2018-05-01 20:28:50 +0000

    Remove zeroupper instruction. We don't use AVX instructions here. Remove deprecated register storage class specifier.

--- gnubg/lib/inputs.c
+++ gnubg/lib/inputs.c
@@ -113,14 +113,14 @@ baseInputs(const TanBoard anBoard, float arInput[])
 
     const unsigned int *pB = &anBoard[0][0];
     float *pInput = &arInput[0];
-    register __m128 vec0;
-    register __m128 vec1;
-    register __m128 vec2;
-    register __m128 vec3;
-    register __m128 vec4;
-    register __m128 vec5;
-    register __m128 vec6;
-    register __m128 vec7;
+    __m128 vec0;
+    __m128 vec1;
+    __m128 vec2;
+    __m128 vec3;
+    __m128 vec4;
+    __m128 vec5;
+    __m128 vec6;
+    __m128 vec7;
 
     while (i--) {
         vec0 = _mm_load_ps(inpvec[*pB++]);
@@ -171,10 +171,6 @@ baseInputs(const TanBoard anBoard, float arInput[])
     /* bar */
     vec0 = _mm_load_ps(inpvecb[*pB]);
     _mm_store_ps(pInput, vec0);
-
-#if defined(USE_AVX)
-    _mm256_zeroupper();
-#endif
 
     return;
 }
