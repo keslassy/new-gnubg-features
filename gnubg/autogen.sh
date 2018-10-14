297d3873b482f0578bc26fff19a259448d146548
plm <>
2018-10-14 20:01:44 +0000

    Avoid warning with libtool-1.x

--- gnubg/autogen.sh
+++ gnubg/autogen.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
+# $Id$
+
 set -x
+
+# If we use libtool-2, libtoolize below will recreate them, but if we use
+# libtool-1 we don't want them, which could happen if we use a shared
+# source directory or work from a "make dist" made on a libtool-2 system.
+#
+rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 
 rm -f aclocal.m4
 aclocal -I m4
@@ -12,11 +20,6 @@ else
     LIBTOOLIZE=libtoolize
 fi
 
-# If we use libtool-2, libtoolize below will recreate them, but if we use
-# libtool-1 we don't want them, which could happen if we use a shared
-# source directory or work from a "make dist" made on a libtool-2 system.
-#
-rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 $LIBTOOLIZE --force --copy
 
 # In case we got a "You should update your `aclocal.m4' by running aclocal."
