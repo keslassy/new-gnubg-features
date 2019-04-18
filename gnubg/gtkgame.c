5fae07b27ac3b770758255e8cc77d591eb2a812c
plm <>
2019-04-18 20:00:48 +0000

    Fix GTKShowScoreSheet() output

--- gnubg/gtkgame.c
+++ gnubg/gtkgame.c
@@ -6064,7 +6064,7 @@ GTKShowScoreSheet(void)
     gtk_tree_view_append_column(GTK_TREE_VIEW(view), column);
 
     renderer = gtk_cell_renderer_text_new();
-    column = gtk_tree_view_column_new_with_attributes(ap[1].szName, renderer, "text", 0, NULL);
+    column = gtk_tree_view_column_new_with_attributes(ap[1].szName, renderer, "text", 1, NULL);
     gtk_tree_view_column_set_min_width(column, 75);
     gtk_tree_view_append_column(GTK_TREE_VIEW(view), column);
 
