00db598c00071740db9d207069d4264bb0066d20
plm <>
2019-11-14 20:08:41 +0000

    Fix typo

--- gnubg/board3d/ShimOGL.c
+++ gnubg/board3d/ShimOGL.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Jon Kinsey <jonkinsey@gmail.com
+ * Copyright (C) 2019 Jon Kinsey <jonkinsey@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
