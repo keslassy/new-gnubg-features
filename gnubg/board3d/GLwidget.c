b8c75566807ab4f66dd61508cdd572d71ca798f1
plm <>
2019-12-06 19:25:06 +0000

    Fix file descriptors leak

--- gnubg/board3d/GLwidget.c
+++ gnubg/board3d/GLwidget.c
@@ -1,4 +1,22 @@
-﻿
+/*
+ * Copyright (C) 2019 Jon Kinsey <jonkinsey@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+
 #include "config.h"
 #ifdef USE_GTK3
 #include <epoxy/gl.h>
@@ -140,13 +158,19 @@ char* LoadFile(const char* filename)
 
 	fseek(fp, 0L, SEEK_END);
 	lSize = ftell(fp);
-	if (lSize == -1)
-		return NULL;
+	if (lSize == -1) {
+                fclose(fp);
+                return NULL;
+        }
+
 	rewind(fp);
 
 	/* allocate memory for entire content */
 	buffer = calloc(1, lSize + 1);
-	if (!buffer) return NULL;
+	if (!buffer) {
+                fclose(fp);
+                return NULL;
+        }
 
 	fread(buffer, lSize, 1, fp);
 
