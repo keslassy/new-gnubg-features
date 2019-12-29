852ffb67a01cc363610de6b29f1803bd4b189c10
plm <>
2019-12-29 15:43:17 +0000

    Add proper copyright attributions as suggested by FSF.

--- gnubg/boarddim.h
+++ gnubg/boarddim.h
@@ -1,11 +1,11 @@
 /*
- * boarddim.h
+ * Copyright (C) 2003 Holger Bochnig <hbgg@gmx.net>
+ * Copyright (C) 2003-2008 the AUTHORS
  *
- * by Holger Bochnig <hbgg@gmx.net>, 2003
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,15 +13,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * $Id$
  */
 
 #ifndef BOARDDIM_H
 #define BOARDDIM_H
-
 
 /* fundamental constants */
 #define CHEQUER_WIDTH     6
@@ -31,7 +29,7 @@
 #define HINGE_HEIGHT     12
 #define HINGE_WIDTH       2
 
-/* additional space (beyone one chequer's worth) between two points 
+/* additional space (beyond one chequer's worth) between two points 
  * with 5 chequers */
 #define EXTRA_HEIGHT      10
 
