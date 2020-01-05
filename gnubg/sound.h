6132f559b5f0bd3e9e67cdfb047f126a37de9c4c
plm <>
2020-01-05 19:51:24 +0000

    Add proper copyright attributions as suggested by FSF.

--- gnubg/sound.h
+++ gnubg/sound.h
@@ -1,11 +1,11 @@
 /*
- * sound.h
+ * Copyright (C) 2002 Joern Thyssen <jthyssen@dk.ibm.com>
+ * Copyright (C) 2002-2008 the AUTHORS
  *
- * by Joern Thyssen <jthyssen@dk.ibm.com>, 2002
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,15 +13,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * $Id$
  */
 
 #ifndef SOUND_H
 #define SOUND_H
-
 
 typedef enum _gnubgsound {
     /* start & exit of gnubg */
