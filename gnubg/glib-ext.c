678a679cf92c5dd284fcdb7c11fbb05fc6d507d6
plm <>
2020-11-08 22:26:24 +0000

    Add proper copyright attributions as suggested by FSF.

--- gnubg/glib-ext.c
+++ gnubg/glib-ext.c
@@ -1,11 +1,10 @@
 /*
- * glib-ext.c -- Map/GList extensions and utility functions for GLIB
+ * Copyright (C) 2014-2015 Michael Petch <mpetch@gnubg.org>
  *
- * by Michael Petch <mpetch@gnubg.org>, 2014.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,11 +12,12 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * $Id$
  */
+
+/* Map/GList extensions and utility functions for GLIB */
 
 #include <config.h>
 #include <errno.h>
@@ -106,9 +106,12 @@ glib_ext_init(void)
     return;
 }
 
+/* GNU indent misformats the next 3 lines, but adding ; is inadequate */
+
 GLIBEXT_DEFINE_BOXED_TYPE(GListBoxed, g_list_boxed, g_list_copy, g_list_gv_boxed_free)
-    GLIBEXT_DEFINE_BOXED_TYPE(GMapBoxed, g_map_boxed, g_list_copy, g_list_gv_boxed_free)
-    GLIBEXT_DEFINE_BOXED_TYPE(GMapEntryBoxed, g_mapentry_boxed, g_list_copy, g_list_gv_boxed_free)
+GLIBEXT_DEFINE_BOXED_TYPE(GMapBoxed, g_map_boxed, g_list_copy, g_list_gv_boxed_free)
+GLIBEXT_DEFINE_BOXED_TYPE(GMapEntryBoxed, g_mapentry_boxed, g_list_copy, g_list_gv_boxed_free)
+
 #if ! GLIB_CHECK_VERSION(2,28,0)
 void
 g_list_free_full(GList * list, GDestroyNotify free_func)
