0354ee2f34177e36f27240c22d8ae8b783809111
plm <>
2022-12-28 20:23:56 +0000

    Raise required minimum GLib version to 2.12.5

--- gnubg/analysis.c
+++ gnubg/analysis.c
@@ -2134,14 +2134,7 @@ cmark_move_set(moverecord * pmr, gchar * sz, CMark cmark)
 
     c = pmr->ml.cMoves;
 
-    /* Mike Petch - This is a hack and is not thread safe under GLIB version < 2.12.5
-     * Was added to allow cygwin to compile */
-
-#if GLIB_CHECK_VERSION(2,12,5)
     while ((n = (int) g_ascii_strtoll(sz, &sz, 10)) != 0) {
-#else
-    while ((n = (int) strtoll(sz, &sz, 10)) != 0) {
-#endif
         if (n > c) {
             outputerrf(_("Only %d legal moves, cannot mark move %d\n"), c, n);
             g_slist_free(list);
